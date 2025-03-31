d90a1c7d00db222ee2ba571f0c473b2626e7c1e3
prateekDazz <146050363+prateekDazz@users.noreply.github.com>
2025-03-31 10:59:33 +0530

    Scheduled annotation

---
+++ ScheduledAnnotation/AppConfig.java
@@ -0,0 +1,21 @@
+package com.udemy.RestApi.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AppConfig {
+	static int i = 0;
+
+	@Scheduled(fixedRate = 5000)
+	public void cleanFile()
+	{
+		i++;
+		System.out.println("cleaning the file::"+i);
+		
+	}
+}

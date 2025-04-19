34dc85a7af278ee1e8cd236ce343ec72cd9e3027
prateekDazz <146050363+prateekDazz@users.noreply.github.com>
2025-04-19 07:58:24 +0530

    concurrenthashmap

--- ConcurrentMap/App_Concurrent.java
+++ ConcurrentMap/App_Concurrent.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -16,87 +18,19 @@ import java.util.stream.Collectors;
 
 public class App_Concurrent {
 	public static void main(String[] args) throws InterruptedException {
-//		Map<Integer, String> map = new HashMap<>();
-		Map<Integer, String> map = new ConcurrentHashMap<Integer, String>();
-     map.put(1, "a");
-     map.put(2, "b");
-     map.put(3, "c");
-//     Map<Integer, String> synMap =  Collections.synchronizedMap(map);This will create an concurrent modification exception
-     
-     Thread t1 = new Thread(()->
-     {
-    	for(Entry<Integer, String>e:map.entrySet())
-    	{
-    	
-    		try {
-				Thread.sleep(100);
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+
+//		Map<String, Integer> map = new HashMap<String, Integer>(); // this will cause concurrent modifcation exception
+		Map<String,Integer>map = new ConcurrentHashMap<String, Integer>();
+		map.put("a", 1);
+		map.put("b", 2);
+		map.put("c", 3);
+
+		for (String s : map.keySet()) {
+			if (s.equals("a")) {
+				map.put("d", 45);
 			}
-    		System.out.println("key Val "+e.getKey()+" value --->"+e.getValue());
-    		
-    	}
-    	 
-     });
+		}
 
-     Thread t2 = new Thread(()->
-     {
-    		try {
-				Thread.sleep(45);
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-//    		synMap.put(45, "ty");
-    		map.put(45, "ty");
-
-    		
-    	
-    	 
-     });
-t1.start();
-t2.start();
-	
 	}
-	
-	public  static <T>int findLength (T[]arr)
-	{
-		
-		return arr.length;
-	}
-	public static  int minOperations(int[] nums, int k) {
-	     
-        int firstMax,secondMax,i;
-        firstMax = -1;
-secondMax = firstMax;
-for(int x:nums)
-{
-    if(x>firstMax)
-    {
-        secondMax=firstMax;
-        firstMax =  Math.max(firstMax,x);
-
-    } 
-    else
-    {
-    	secondMax =  Math.max(secondMax, x);
-    }
-}
-for(i=0;i<nums.length;i++)
-{
-    if(nums[i]==firstMax)
-    {
-        nums[i]=secondMax;
-    }
-    if(nums[i]<k)
-    {
-        return -1;
-    }
-}
-Map<Integer,Long>map =  		Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));		
-	long z =  map.entrySet().stream().map(e->e.getKey()).filter(x->x>k).count();
-    return (int)z;
-    }
 
 }
